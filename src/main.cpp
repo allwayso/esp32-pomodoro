002f188d0f768c200214fff0df1388e0ecbdbc03
allwayso <1784817625@qq.com>
2026-09-19 22:09:58 +0800

    feat:add joystick butten test

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,48 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7789.h>
+  #include <Arduino.h>
+  #include <SPI.h>
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_ST7789.h>
 
-constexpr int TFT_CS = 5;    // D5
-constexpr int TFT_DC = 17;   // TX2
-constexpr int TFT_RST = 16;  // RX2
+  constexpr int TFT_CS = 5;
+  constexpr int TFT_DC = 17;
+  constexpr int TFT_RST = 16;
+  constexpr int BUTTON_PIN = 32;
 
-Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
+  Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
 
-void setup() {
-  Serial.begin(115200);
+  void drawButtonState(bool pressed) {
+    tft.fillScreen(pressed ? ST77XX_GREEN : ST77XX_BLUE);
 
-  SPI.begin(18, -1, 23, 5);
-  tft.init(240, 240);
-  tft.setRotation(0);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(3);
+    tft.setCursor(35, 90);
+    tft.print(pressed ? "PRESSED" : "RELEASED");
+  }
 
-  tft.fillScreen(ST77XX_RED);
-  delay(1000);
-  tft.fillScreen(ST77XX_GREEN);
-  delay(1000);
-  tft.fillScreen(ST77XX_BLUE);
+  void setup() {
+    Serial.begin(115200);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(3);
-  tft.setCursor(55, 105);
-  tft.print("TFT OK");
-}
+    SPI.begin(18, -1, 23, 5);
+    tft.init(240, 240);
+    tft.setRotation(0);
 
-void loop() {
-}
+    drawButtonState(false);
+    Serial.println("Joystick button test ready");
+  }
+
+  void loop() {
+    static bool previousPressed = false;
+    bool pressed = digitalRead(BUTTON_PIN) == LOW;
+
+    if (pressed != previousPressed) {
+      delay(30);
+      pressed = digitalRead(BUTTON_PIN) == LOW;
+
+      if (pressed != previousPressed) {
+        previousPressed = pressed;
+        drawButtonState(pressed);
+        Serial.println(pressed ? "PRESSED" : "RELEASED");
+      }
+    }
+  }
